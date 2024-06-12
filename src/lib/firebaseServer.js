7c8a61041eb96d8a59241391c8cc3739b715a5b0
Gabriel Gutierrez <77996586+gg447062@users.noreply.github.com>
2024-06-12 17:19:02 -0600

    add hack for avoiding multiple firebase initializeApp calls

--- src/lib/firebaseServer.js
+++ src/lib/firebaseServer.js
@@ -1,14 +1,22 @@
 // add server-only package to generate build time errors when not used on server
 
-const { initializeApp, cert } = require('firebase-admin/app');
+const { initializeApp, cert, getApps } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
 
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
+const config = { credential: cert(serviceAccount) };
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
-});
+const getOrInitialzeApp = () => {
+  const apps = getApps();
+  const app = apps.length == 0 ? initializeApp(config) : apps[0];
+  return app;
+};
+
+const app =
+  process.env.NODE_ENV === 'production'
+    ? initializeApp(config)
+    : getOrInitialzeApp();
 
 const db = getFirestore();
 const auth = getAuth(app);
