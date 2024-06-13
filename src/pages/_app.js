c5179411f94c155df7d27e0717a9786d8fab9e77
Gabriel Gutierrez <77996586+gg447062@users.noreply.github.com>
2024-06-13 11:20:11 -0600

    add fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 'use client';
 import '@/styles/globals.css';
 import '@/styles/main.css';
+import '@/styles/fonts.css';
 import { EditorProvider } from '@/lib/contexts/editorContext';
 import { MetadataProvider } from '@/lib/contexts/metadataContext';
 import { UserProvider } from '@/lib/contexts/userContext';
